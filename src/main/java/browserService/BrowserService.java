d081e6b465da8e6d8b3133cd2a738b715144f337
Olga Yukovets <yuks230990@gmail.com>
2020-12-26 14:55:31 +0300

    init Base classes

--- src/main/java/browserService/BrowserService.java
+++ src/main/java/browserService/BrowserService.java
@@ -12,30 +12,25 @@ public class BrowserService {
     private DriverManagerType driverManagerType = null;
 
     public BrowserService() {
-        String browserName = new ReadProperties().getBrowserName();
-
+        ReadProperties readProperties = new ReadProperties();
+        String browserName = readProperties.getBrowserName();
         switch (browserName.toLowerCase()) {
             case "chrome":
-
                 driverManagerType = DriverManagerType.CHROME;
                 WebDriverManager.getInstance(driverManagerType).setup();
 
-                System.setProperty("webdriver.chrome.driver", "C:/Drivers/chromedriver.exe");
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--disable-gpu");
                 chromeOptions.addArguments("--start-maximized");
-                chromeOptions.setHeadless(new ReadProperties().isHeadless());
+                chromeOptions.setHeadless(readProperties.isHeadless());
 
                 driver = new ChromeDriver(chromeOptions);
                 driver.manage().window().maximize();
                 break;
-
-            default:
-                System.out.println("Browser is not supported.");
-                break;
         }
     }
     public WebDriver getDriver() {
+
         return driver;
     }
 }
