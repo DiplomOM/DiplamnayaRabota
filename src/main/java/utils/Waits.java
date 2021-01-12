b99f59d4d069ea7f94c58dbf6a3f0ca1850528a7
Olga Yukovets <yuks230990@gmail.com>
2021-01-12 22:41:11 +0300

    fix upload test split test cases

--- src/main/java/utils/Waits.java
+++ src/main/java/utils/Waits.java
@@ -2,22 +2,26 @@ package utils;
 
 import browserService.ReadProperties;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Waits {
-    private WebDriver driver;
+
     private WebDriverWait webDriverWait;
 
 
     public Waits(WebDriver driver, int timeOut) {
-        this.driver = driver;
         webDriverWait = new WebDriverWait(driver, timeOut);
     }
 
     public Waits(WebDriver driver) {
-        this.driver = driver;
         ReadProperties readProperties = new ReadProperties();
         webDriverWait = new WebDriverWait(driver, readProperties.getTimeOut());
+    }
+
+    public void waitUntilRedrawDOM(WebElement element) {
+        webDriverWait.until(ExpectedConditions.stalenessOf(element));
     }
 
 }
